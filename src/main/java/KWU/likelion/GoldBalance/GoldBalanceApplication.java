c793d9a6a30d148340fa8fc0589138408a428180
pyungjong0314 <sonopj@gmail.com>
2024-04-11 15:24:46 +0900

    first upload

---
+++ src/main/java/KWU/likelion/GoldBalance/GoldBalanceApplication.java
@@ -0,0 +1,13 @@
+package KWU.likelion.GoldBalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldBalanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KWU/likelion/GoldBalance/GoldBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package KWU.likelion.GoldBalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
