0dbee47e17067116fab6dfab089b6714c268ee78
cjh-19 <gns8812@naver.com>
2024-04-14 15:39:35 +0900

    service update
    1. service 레이어 인터페이스와 구현체(스켈레톤만) 작성
    2. 경민 domain, dto 참고하여 작성

---
+++ src/main/java/KWU/likelion/GoldBalance/service/CommentServiceImpl.java
@@ -0,0 +1,30 @@
+package KWU.likelion.GoldBalance.service;
+
+import KWU.likelion.GoldBalance.domain.Comment;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentServiceImpl implements CommentService{
+
+    @Override
+    public Comment createComment(Comment comment) {
+        return null;
+    }
+
+    @Override
+    public Comment getComment(Long commentId) {
+        return null;
+    }
+
+    @Override
+    public List<Comment> getAllComment() {
+        return null;
+    }
+
+    @Override
+    public Comment likeComment(Long commentId) {
+        return null;
+    }
+}
