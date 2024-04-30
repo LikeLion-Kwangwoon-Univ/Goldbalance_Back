57a5bcab4b9a105f7e57fb735c89a2986433e903
cjh-19 <gns8812@naver.com>
2024-04-30 23:58:58 +0900

    method implement
    createComment, getComment method implementation

--- src/main/java/KWU/likelion/GoldBalance/service/CommentServiceImpl.java
+++ src/main/java/KWU/likelion/GoldBalance/service/CommentServiceImpl.java
@@ -1,6 +1,8 @@
 package KWU.likelion.GoldBalance.service;
 
 import KWU.likelion.GoldBalance.domain.Comment;
+import KWU.likelion.GoldBalance.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,18 +10,28 @@ import java.util.List;
 @Service
 public class CommentServiceImpl implements CommentService{
 
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentServiceImpl(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
     @Override
     public Comment createComment(Comment comment) {
-        return null;
+        // save 메소드를 사용하여 Comment 객체를 데베에 저장하고 반환
+        return commentRepository.save(comment);
     }
 
     @Override
-    public Comment getComment(Long commentId) {
-        return null;
+    public Comment getComment(int commentId) {
+        // commentId에 해당하는 Comment 객체를 데베에서 조회 -> 없다면 예외처리
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
     }
 
     @Override
-    public Comment likeComment(Long commentId) {
+    public Comment likeComment(int commentId) {
         return null;
     }
 
@@ -29,7 +41,7 @@ public class CommentServiceImpl implements CommentService{
     }
 
     @Override
-    public List<Comment> getAllChildComment(Long commentId) {
+    public List<Comment> getAllChildComment(int commentId) {
         return null;
     }
 
