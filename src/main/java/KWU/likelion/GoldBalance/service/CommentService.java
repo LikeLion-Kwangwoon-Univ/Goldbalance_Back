4474b04077c4efa941b9f36265eb415e6ce0b449
cjh-19 <gns8812@naver.com>
2024-05-12 17:11:15 +0900

    update commnetservice
    내역: commentService의 getAllComment 파라미터 재구성

--- src/main/java/KWU/likelion/GoldBalance/service/CommentService.java
+++ src/main/java/KWU/likelion/GoldBalance/service/CommentService.java
@@ -15,8 +15,8 @@ public interface CommentService {
     // 댓글 좋아요 메소드
     Comment likeComment(int commentId);
 
-    // 모든 댓글 조회 메소드
-    List<Comment> getAllComment();
+    // post_id에 해당하는 모든 댓글 조회 메소드
+    List<Comment> getAllComment(int postId);
 
     // 특정 댓글의 모든 대댓글 조회 메소드
     List<Comment> getAllChildComment(int commentId);
