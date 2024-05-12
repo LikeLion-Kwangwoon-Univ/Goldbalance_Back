fef0e79063a320b26d12008e093c7f1e01918b37
kymin <park48664866@gmail.com>
2024-05-12 22:34:23 +0900

    Fix: modify domains
    modify domains:
    - add @column
    - fix oneToMany

--- src/main/java/KWU/likelion/GoldBalance/domain/Comment.java
+++ src/main/java/KWU/likelion/GoldBalance/domain/Comment.java
@@ -15,30 +15,35 @@ public class Comment {  // comment table과 mapping될 도메인
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     // commentId
+    @Column(name = "id")
     private int id;
 
     // comment가 위치해 있는 post의 id
+    @Column(name = "postId")
     private int postId;
 
-    // comment작성자가 설정한 비밀번호
-    private String password;
-
     // comment내용
+    @Column(name = "content")
     private String content;
     
     @CreatedDate
     // comment가 달린 시각
+    @Column(name = "createdDateTime")
     private LocalDateTime createdDateTime;
     
     // comment의 좋아요 수
+    @Column(name = "likeCount")
     private int likeCount;
     
     // post에 속한 두 개의 오브젝트 중 어느 쪽인지 표시
+    @Column(name = "sideInfo")
     private int sideInfo;
     
     // 대댓글의 수
+    @Column(name = "childCount")
     private int childCount;
     
     // 현재 comment가 댓글이라면 -1, 대댓글이라면 댓글의 id
+    @Column(name = "parentCommentId")
     private int parentCommentId;
 }
--- src/main/java/KWU/likelion/GoldBalance/domain/Post.java
+++ src/main/java/KWU/likelion/GoldBalance/domain/Post.java
@@ -16,44 +16,44 @@ public class Post { // post table과 mapping될 도메인
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     // post의 id
+    @Column(name = "id")
     private int id;
 
     // post의 제목 (ex. 피자 vs 치킨)
+    @Column(name = "postTitle")
     private String postTitle;
-
-    // post 생성자가 설정한 비밀번호
-    private String password;
 
     @CreatedDate
     // post가 생성된 시각
+    @Column(name = "createdDateTime")
     private LocalDateTime createdDateTime;
 
     // vs의 대상 중 왼쪽 개체의 제목
+    @Column(name = "leftSideTitle")
     private String leftSideTitle;
 
     // vs의 대상 중 왼쪽 개체의 설명
+    @Column(name = "leftSideDetail")
     private String leftSideDetail;
 
-    // vs의 대상 중 왼쪽 개체의 이미지 경로
-    private String leftSideImage;
-
     // vs의 대상 중 왼쪽 개체의 득표수
+    @Column(name = "leftSideVote")
     private int leftSideVote;
 
     // vs의 대상 중 오른쪽 개체의 제목
+    @Column(name = "rightSideTitle")
     private String rightSideTitle;
 
     // vs의 대상 중 오른쪽 개체의 설명
+    @Column(name = "rightSideDetail")
     private String rightSideDetail;
 
-    // vs의 대상 중 오른쪽 개체의 이미지 경로
-    private String rightSideImage;
-
     // vs의 대상 중 오른쪽 개체의 득표수
+    @Column(name = "rightSideVote")
     private int rightSideVote;
 
     // 댓글 연결 -> 아래에 구현된 코드는 댓글 대댓글 구분 없이 같은 postId면 다 불러옴...
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "post_id")
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "postId")
     private List<Comment> commentList;
 }
