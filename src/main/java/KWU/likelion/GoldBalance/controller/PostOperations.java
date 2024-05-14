27d6f25f810fd2ce5066b4d146166b60d30bfa82
5jun99 <oj041088@gmail.com>
2024-05-14 11:02:08 +0900

    update controller

--- src/main/java/KWU/likelion/GoldBalance/controller/PostOperations.java
+++ src/main/java/KWU/likelion/GoldBalance/controller/PostOperations.java
@@ -12,19 +12,18 @@ import java.util.List;
 @RequestMapping("/default") //투표 게시물에 대한 controller
 public interface PostOperations {
 
-    @GetMapping("/comment_count") //처음에 모든 정보를 받아 투표의 list를 반환함 - 댓글 순
-    ResponseEntity<PostList> getPostsSortedByCommentCount();
-
-    @GetMapping("/vote_count") //처음에 모든 정보를 받아 투표의 list를 반환함 - 투표 순
-    ResponseEntity<PostList> getPostsSortedByVoteCount();
+//    @GetMapping("/comment_count") //처음에 모든 정보를 받아 투표의 list를 반환함 - 댓글 순
+//    ResponseEntity<PostList> getPostsSortedByCommentCount();
+    @GetMapping("/hot") //처음에 모든 정보를 받아 투표의 list를 반환함 - 투표 순
+    ResponseEntity<PostList> getPostsSortedByVoteCount(@RequestParam Integer cursor);
 
     @GetMapping("/latest") //처음에 모든 정보를 받아 투표의 list를 반환함 - 시간 순
-    ResponseEntity<PostList> getPostsSortedByTime();
-    @GetMapping("/closest") // 5퍼 내로 차이나는 게시물을 불러올 수 있는 메서드
-    ResponseEntity<PostList> getPostsbyCloseVote();
+    ResponseEntity<PostList> getPostsSortedByTime(@RequestParam Integer cursor);
+    @GetMapping("/balance") // 5퍼 내로 차이나는 게시물을 불러올 수 있는 메서드
+    ResponseEntity<PostList> getPostsbyCloseVote(@RequestParam Integer cursor);
     @PostMapping("/make") //투표 게시물 저장
     ResponseEntity<String> submitPost(@RequestBody MakePost makePost);
 
-    @GetMapping("{id}/select") //아이디데 대한 post 전달
+    @GetMapping("{id}") //아이디데 대한 post 전달
     ResponseEntity<PostInfo> getPostById(@PathVariable String id);
 }
