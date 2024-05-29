7281deaa7c69b9947e9d27ccc700f0b30c5174cd
5jun99 <oj041088@gmail.com>
2024-05-30 00:23:03 +0900

    fix: VoteOperation 수정
    vote 결과 저장할 때 클라이언트에서 body를 주는 것은 아니어서 postmapping 이 아닌 getmapping 하기로 함

--- src/main/java/KWU/likelion/GoldBalance/controller/VoteOperations.java
+++ src/main/java/KWU/likelion/GoldBalance/controller/VoteOperations.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public interface VoteOperations {
 
     @GetMapping("/{id}/{selectSide}") // 투표 하기
-    ResponseEntity<String> vote(@PathVariable String id, @PathVariable String selectNum);
+    ResponseEntity<String> vote(@PathVariable String id, @PathVariable String selectSide);
     @GetMapping("/{id}")    //해당 포스트에 대한 투표 결과(VoteResult 객체에) 주기 ex> 왼쪽 5표 오른쪽 8표 이런식으로
     ResponseEntity<VoteResult> getVoteResult(@PathVariable String id);
 }
